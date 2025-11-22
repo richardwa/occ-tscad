b52576680a21342e40373a8192b07d3ecfbdc2a1
rich <rich@lan>
2025-11-22 10:48:43 -0500

    update

--- models/sample.ts
+++ models/sample.ts
@@ -1,4 +1,4 @@
-import { Box, Sphere } from "../src/common/csg/primitives";
+import { Box, Sphere } from "../src/common/csg";
 
 export const main = () =>
-  new Box(2, 2, 2).union(new Box(2, 2, 2).translate([1, 1, 1]), 0.2);
+  new Box(2, 2, 2).intersect({radius:0.2, target: new Box(2, 2, 2).translate([1, 1, 1])});
---
+++ src/common/csg/shape3.ts
@@ -0,0 +1,151 @@
+import {
+  OpenCascadeInstance,
+  TopoDS_Shape,
+  TopoDS_Edge,
+  TopTools_ListOfShape,
+  TopAbs_ShapeEnum,
+  ChFi3d_FilletShape,
+  Message_ProgressRange,
+  ProjLib_PrjResolve,
+  BRepAlgoAPI_Fuse_3,
+} from "opencascade.js";
+import { getOCC } from "../../client/app/occ";
+
+const oc = getOCC();
+
+const TopAbs_EDGE: TopAbs_ShapeEnum = oc.TopAbs_ShapeEnum.TopAbs_EDGE as any;
+const TopAbs_SHAPE: TopAbs_ShapeEnum = oc.TopAbs_ShapeEnum.TopAbs_SHAPE as any;
+const ChFi3d_Rational: ChFi3d_FilletShape = oc.ChFi3d_FilletShape
+  .ChFi3d_Rational as any;
+const progress = new oc.Message_ProgressRange_1();
+
+export type WithRadius<T> = {
+  radius: number;
+  type?: "fillet" | "chamfer";
+  target: T;
+};
+export class Shape3 {
+  shape: TopoDS_Shape;
+  constructor(shape: TopoDS_Shape) {
+    this.shape = shape;
+  }
+  scale(x: number) {
+    const tf = new oc.gp_Trsf_1();
+    tf.SetScaleFactor(x);
+    const loc = new oc.TopLoc_Location_2(tf);
+    this.shape = this.shape.Moved(loc, false);
+    return this;
+  }
+
+  translate([x, y, z]: number[]) {
+    const tf = new oc.gp_Trsf_1();
+    tf.SetTranslation_1(new oc.gp_Vec_4(x, y, z));
+    const loc = new oc.TopLoc_Location_2(tf);
+    this.shape = this.shape.Moved(loc, false);
+    return this;
+  }
+
+  private handleSectionEdges(
+    props: WithRadius<Shape3>,
+    fuse: BRepAlgoAPI_Fuse_3,
+  ) {
+    const fusedShape = fuse.Shape();
+
+    const mkFillet = new oc.BRepFilletAPI_MakeFillet(
+      fusedShape,
+      ChFi3d_Rational,
+    );
+    const newEdgesList = fuse.SectionEdges();
+
+    while (!newEdgesList.IsEmpty()) {
+      const edgeShape = newEdgesList.First_1();
+      newEdgesList.RemoveFirst(); // remove it from list
+
+      if (edgeShape.ShapeType() === oc.TopAbs_ShapeEnum.TopAbs_EDGE) {
+        const edge = oc.TopoDS.Edge_1(edgeShape);
+        mkFillet.Add_2(props.radius, edge);
+      }
+    }
+
+    mkFillet.Build(progress);
+    return mkFillet.Shape();
+  }
+
+  union(prop: Shape3 | WithRadius<Shape3>) {
+    const fillProps: WithRadius<Shape3> =
+      "target" in prop ? prop : { radius: 0, type: "fillet", target: prop };
+    const fuse = new oc.BRepAlgoAPI_Fuse_3(
+      this.shape,
+      fillProps.target.shape,
+      progress,
+    );
+    fuse.Build(progress);
+
+    if (fillProps.radius) {
+      this.shape = this.handleSectionEdges(fillProps, fuse);
+    } else {
+      this.shape = fuse.Shape();
+    }
+    return this;
+  }
+
+  cut(prop: Shape3 | WithRadius<Shape3>) {
+    const fillProps: WithRadius<Shape3> =
+      "target" in prop ? prop : { radius: 0, type: "fillet", target: prop };
+    const cut = new oc.BRepAlgoAPI_Cut_3(
+      this.shape,
+      fillProps.target.shape,
+      progress,
+    );
+    cut.Build(progress);
+    if (fillProps.radius) {
+      this.shape = this.handleSectionEdges(fillProps, cut);
+    } else {
+      this.shape = cut.Shape();
+    }
+    return this;
+  }
+
+  intersect(prop: Shape3 | WithRadius<Shape3>) {
+    const fillProps: WithRadius<Shape3> =
+      "target" in prop ? prop : { radius: 0, type: "fillet", target: prop };
+    const intersect = new oc.BRepAlgoAPI_Common_3(
+      this.shape,
+      fillProps.target.shape,
+      progress,
+    );
+    intersect.Build(progress);
+    if (fillProps.radius) {
+      this.shape = this.handleSectionEdges(fillProps, intersect);
+    } else {
+      this.shape = intersect.Shape();
+    }
+    return this;
+  }
+
+  private doEdges(fn: (edge: TopoDS_Edge, i: number) => void) {
+    const edgeMap = new oc.TopTools_IndexedMapOfShape_1();
+    oc.TopExp.MapShapes_1(this.shape, TopAbs_EDGE, edgeMap);
+    for (let i = 1; i <= edgeMap.Extent(); i++) {
+      const edge = oc.TopoDS.Edge_1(edgeMap.FindKey(i));
+      fn(edge, i - 1);
+    }
+  }
+
+  fillet(radius: number, edges?: number[]) {
+    const mkFillet = new oc.BRepFilletAPI_MakeFillet(
+      this.shape,
+      ChFi3d_Rational,
+    );
+
+    const edgeSet = edges ? new Set(edges) : undefined;
+    this.doEdges((edge, i) => {
+      if (!edgeSet || edgeSet.has(i)) {
+        mkFillet.Add_2(radius, edge);
+      }
+    });
+    mkFillet.Build(progress);
+    this.shape = mkFillet.Shape();
+    return this;
+  }
+}
