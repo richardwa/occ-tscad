638e5b2b3716e16f277f36aaf88504ca1f8476f2
rich <rich@lan>
2025-11-28 19:25:58 -0500

    update

--- src/client/app/modelviewer.ts
+++ src/client/app/modelviewer.ts
@@ -1,7 +1,11 @@
 import { hbox, vbox, div, fragment, grid } from "../lib/base-components";
 import { Button, NumberInput, Title } from "./components";
 import { signal, h, downloadBinaryFile } from "../lib";
-import { shapeToGLB, glbToStlUrl, glbToObjUrl } from "./shapeToUrl";
+import {
+  shapeToGLB,
+  glbToStlUrl,
+  glbToObjUrl,
+} from "../../common/csg/shapeToUrl";
 import { setExtension } from "../../common/util";
 import "@google/model-viewer";
 import { modelUrl } from "./model-store";
@@ -9,26 +13,29 @@ import { modelUrl } from "./model-store";
 export const ModelViewer = (file: string) => {
   const initialDirection = signal("45deg 75deg auto");
 
-  return vbox().css("flex-grow","1").inner(
-    hbox().inner(
-      Button()
-        .on("click", () => initialDirection.trigger())
-        .inner("Reset View"),
-      Button()
-        .on("click", async () => {
-          const url = modelUrl.get();
-          if (url) {
-            const stlUrl = await glbToObjUrl(url);
-            downloadBinaryFile(stlUrl, setExtension(file, "obj"));
-          }
-        })
-        .inner("Download Obj"),
-    ),
-    h("model-viewer")
-      .attr("camera-controls")
-      .attr("interaction-prompt", "none")
-      .attr("camera-orbit", initialDirection)
-      .attr("src", modelUrl)
-      .css("height", "100%").css("width","100%"),
-  );
+  return vbox()
+    .css("flex-grow", "1")
+    .inner(
+      hbox().inner(
+        Button()
+          .on("click", () => initialDirection.trigger())
+          .inner("Reset View"),
+        Button()
+          .on("click", async () => {
+            const url = modelUrl.get();
+            if (url) {
+              const stlUrl = await glbToObjUrl(url);
+              downloadBinaryFile(stlUrl, setExtension(file, "obj"));
+            }
+          })
+          .inner("Download Obj"),
+      ),
+      h("model-viewer")
+        .attr("camera-controls")
+        .attr("interaction-prompt", "none")
+        .attr("camera-orbit", initialDirection)
+        .attr("src", modelUrl)
+        .css("height", "100%")
+        .css("width", "100%"),
+    );
 };
