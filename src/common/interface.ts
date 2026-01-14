ac63abd86329c00afe44ff0986abaa1e921ad157
rich <rich@lan>
2026-01-13 21:02:40 -0500

    update

--- src/common/interface.ts
+++ src/common/interface.ts
@@ -7,9 +7,25 @@ export type ServerApi = {
 export const fetchJson = <T extends keyof ServerApi>(
   key: T,
   ...params: Parameters<ServerApi[T]>
-) =>
-  fetch(`${apiPath}/${key}`, {
+) => {
+  // handle demo mode in github pages
+  if (document.location.hostname.endsWith("github.io")) {
+    if (key === "listFiles") {
+      return [
+        "test-2d.ts",
+        "test-cut.ts",
+        "test-fillet.ts",
+        "test-intersect.ts",
+        "test-revolve.ts",
+        "test-union.ts",
+        "test-xyz.ts",
+      ];
+    }
+  }
+
+  return fetch(`${apiPath}/${key}`, {
     method: "post",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(params),
   }).then((res) => res.json()) as ReturnType<ServerApi[T]>;
+};
