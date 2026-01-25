e7a7577b20f737a06af16e4cf52b07bac4f5eca0
Richard W <rw>
2026-01-25 00:41:16 -0500

    update

--- occ-tscad-view/src/routes.ts
+++ occ-tscad-view/src/routes.ts
@@ -5,12 +5,11 @@ import { CodePad } from "./codepad";
 
 const base = new URL(document.baseURI).pathname.split("/")[1];
 const router = new Router(base);
-console.log({base})
+console.log({ base });
 
 router.addRoute("/", () => Title("Select File"));
-router.addRoute("/model-viewer/:file", (params) =>{
-
-console.log(params)
+router.addRoute("/model-viewer/:file", (params) => {
+  console.log(params);
   return hbox()
     .css("flex-grow", "1")
     .inner(CodePad(params.file), ModelViewer(params.file));
