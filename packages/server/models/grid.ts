b28e14ffa8cce04284887bbe8c7d55c03ff223f8
Richard W <rw>
2026-02-16 09:05:45 -0500

    update

--- packages/server/models/grid.ts
+++ packages/server/models/grid.ts
@@ -2,9 +2,8 @@ import { box, sphere, circle, Vec2 } from "occ-tscad";
 
 type GridProps = {
   thickness: number;
-  border: number;
-  rows: number;
-  cols: number;
+  border: Vec2;
+  rowsCols: Vec2;
   keySize: Vec2;
   gap: Vec2;
 };
@@ -12,31 +11,29 @@ type GridProps = {
 export const grid = ({
   thickness,
   border,
-  rows,
-  cols,
+  rowsCols,
   keySize,
   gap,
 }: GridProps) => {
   const totalKeySize = [keySize[0] + gap[0], keySize[1] + gap[1]];
   return box(
-    2 * border + cols * totalKeySize[0] - gap[0],
-    2 * border + rows * totalKeySize[1] - gap[1],
+    2 * border[0] + rowsCols[1] * totalKeySize[0] - gap[0],
+    2 * border[1] + rowsCols[0] * totalKeySize[1] - gap[1],
     thickness,
     false,
   ).cut(
     box(keySize[0], keySize[1], thickness * 2, false)
-      .translate([border, border, -thickness])
-      .repeat(cols, [totalKeySize[0], 0, 0])
-      .repeat(rows, [0, totalKeySize[1], 0]),
+      .translate([border[0], border[1], -thickness])
+      .repeat(rowsCols[1], [totalKeySize[0], 0, 0])
+      .repeat(rowsCols[0], [0, totalKeySize[1], 0]),
   );
 };
 
 export const main = () =>
   grid({
     thickness: 2,
-    border: 10,
-    rows: 4,
-    cols: 8,
-    keySize: [19, 19],
-    gap: [5, 5],
+    border: [10,2],
+    rowsCols: [4, 8],
+    keySize: [25, 19],
+    gap: [10, 5],
   });
